5a90f6e5b56b2929a76aba9ad7de0aa62d23ba30
ItsBean <965106195@qq.com>
2026-08-26 07:17:35 +0000

    Update project page and authors

--- assets/site.js
+++ assets/site.js
@@ -11,6 +11,11 @@ const project = {
       email: "00033521@whu.edu.cn",
     },
     {
+      name: "Jinrong Yang",
+      affiliation: "Huazhong University of Science and Technology",
+      email: "yangjinrong@hust.edu.cn",
+    },
+    {
       name: "Zhaojie Chu",
       affiliation: "School of Electronic and Information Engineering, South China University of Technology",
       email: "zjchu_china@163.com",
